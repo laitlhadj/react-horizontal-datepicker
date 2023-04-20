abe63191315bbe059231c615aac1d5d07932a3ef
laitlhadj <laurent.aitlhadj@gmail.com>
2023-04-20 22:05:26 +0200

    more smooth

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -177,7 +177,7 @@ export const Datepicker = forwardRef<HTMLDivElement, DatepickerProps>(
     const nextScroll = () => {
       if (containerRef.current) {
         containerRef.current.scrollBy({
-          left: +500,
+          left: +100,
           behavior: "smooth",
         });
       }
@@ -186,7 +186,7 @@ export const Datepicker = forwardRef<HTMLDivElement, DatepickerProps>(
     const prevScroll = () => {
       if (containerRef.current) {
         containerRef.current.scrollBy({
-          left: -500,
+          left: -100,
           behavior: "smooth",
         });
       }
